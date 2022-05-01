f095c491d347cefa9e71e00b36af4fface662b01
Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>
2022-05-01 17:30:55 +0200

    Console 0.0.2 & Emojis 0.0.1
    Signed-off-by: Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 import express from 'express'
-const app = express()
-
-import Logger from './lib/console/Logger.js'
-const logger = new Logger( 'Server', { replace: '><', text_format: { style: 'blink', fg: 'blue', bg: 'white' } } )
-
 import { resolve } from 'path'
-
+import Logger from './lib/console/Logger.js'
+import TestConsole from './lib/console/TestConsole.js'
+import Emojis from './lib/emojis/Emojis.js'
 import indexRouter from './routes/indexRouter.js'
 import usersRouter from './routes/usersRouter.js'
 
-import TestConsole from './lib/console/TestConsole.js'
-const test = new TestConsole( 32, false, true )
-
-import Emojis from './lib/emojis/Emojis.js'
+const app = express()
+const logger = new Logger('Server', '><', true)
+const test = new TestConsole(32, false, true)
 const emojis = new Emojis()
 
-app.use( express.json() )
-app.use( express.static( resolve( './public' ) ) )
-app.use( express.static( resolve( './frontend/public' ) ) )
+app.use(express.json())
+app.use(express.static(resolve('./public')))
+app.use(express.static(resolve('./frontend/public')))
 
-app.use( '/', indexRouter )
-app.use( '/api/users', usersRouter )
+app.use('/', indexRouter)
+app.use('/api/users', usersRouter)
 
 // error handler
-app.use( ( _req, res, _next ) => {
-    res.sendStatus( 403 )
-} )
+app.use((_req, res, _next) => {
+    res.sendStatus(403)
+})
 
 const port = process.env.PORT || 3000
-const server = app.listen( port, () => {
-    logger.log(
-        'info', 'Listening on port ><.',
-        { values: [ port.toString() ] },
-        true )
-} )
+const server = app.listen(port, () => {
+    logger.info('Listening on port ><.', [port.toString()])
+})
